f6408e62fec6c75fd94e53e46fdcf7fb17ed5124
Meghraj Suthar <iamromec@gmail.com>
2022-06-19 20:06:04 +0530

    fixes error

--- src/Message.php
+++ src/Message.php
@@ -8,19 +8,19 @@ class Message extends Entity
 {
     public function sendMessage($attributes = array())
     {
-        $relativeUrl = '/send/message';
+        $relativeUrl = 'send/message';
         return $this->request('POST', $relativeUrl, $attributes);
     }
 
     public function sendTemplate($attributes = array())
     {
-        $relativeUrl = '/send/template';
+        $relativeUrl = 'send/template';
         return $this->request('POST', $relativeUrl, $attributes);
     }
 
     public function sendMedia($attributes = array())
     {
-        $relativeUrl = '/send/media';
+        $relativeUrl = 'send/media';
         return $this->request('POST', $relativeUrl, $attributes);
     }
 }
